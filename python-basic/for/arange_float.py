5878210ba734ab2ea4408d4b8bfba1d80e83d5da
nkhn37 <nkhn37.github@gmail.com>
2022-01-28 07:56:41 +0900

    for文基礎 ソースコード（range関数）
    range関数を用いた場合のfor文の使い方に関するソースコードサンプル

---
+++ python-basic/for/arange_float.py
@@ -0,0 +1,14 @@
+"""for文基礎
+float型の数値シーケンスで繰り返し処理を行う場合
+numpyパッケージのarange関数を使用する
+
+※ numpyパッケージがインストールされていない場合は以下のようにpipでのインストールが必要
+pip install numpy
+
+[説明ページ]
+https://tech.nkhn37.net/python-for-range/#range
+"""
+import numpy as np
+
+for i in np.arange(0.5, 5.5, 0.5):
+    print(i)
