bd056a576221266336c405e870044fc4144e5f95
nkhn37 <nkhn37.github@gmail.com>
2022-01-29 14:34:18 +0900

    for文基礎 コメント修正
    range, enumerate, zipを当初「関数」とコメントしてしまっていたが、実態としてはイテラブルなオブジェクトを返すクラスであり誤りであるため、コメントを修正。

--- python-basic/for/enumerate.py
+++ python-basic/for/enumerate.py
@@ -1,11 +1,11 @@
 """for文基礎
-enumerate関数を用いたfor文の使い方
+enumerateを用いたfor文の使い方
 （基本的な使い方）
 
 [説明ページ]
 https://tech.nkhn37.net/python-for-enumerate/#enumeratefor
 """
-# enumerate関数
+# enumerate
 data = ['A', 'B', 'C', 'D', 'E']
 
 for idx, dt in enumerate(data):
--- python-basic/for/enumerate_from_any_number.py
+++ python-basic/for/enumerate_from_any_number.py
@@ -1,10 +1,10 @@
 """for文基礎
-enumerate関数で任意のインデックス数値から開始する方法
+enumerateで任意のインデックス数値から開始する方法
 
 [説明ページ]
 https://tech.nkhn37.net/python-for-enumerate/#enumerate
 """
-# enumerate関数のインデックス開始数値を指定する
+# enumerateのインデックス開始数値を指定する
 data = ['A', 'B', 'C', 'D', 'E']
 
 for idx, dt in enumerate(data, 5):
