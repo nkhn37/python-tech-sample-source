c7676bc85b032243676f684d58b8b67e8ea5757c
nkhn37 <nkhn37.github@gmail.com>
2022-02-27 06:37:57 +0900

    collections ソースコード （deque）
    collectionsモジュールのdequeの使用方法（キューとスタックの実現）に関するソースコードサンプル

---
+++ python-libraries/collections/deque/deque_stack.py
@@ -0,0 +1,24 @@
+"""Collectionsモジュール
+dequeでキューやスタックを実現する
+スタック（stack）操作の実現
+
+[説明ページ]
+https://tech.nkhn37.net/python-collections-deque/#stack
+"""
+import collections
+
+# dequeの作成
+data = collections.deque()
+
+# データの追加
+data.append(10)
+data.append(20)
+data.append(30)
+data.append(40)
+
+# 末尾からのデータ取り出し
+print(data)
+print(data.pop())
+print(data)
+print(data.pop())
+print(data)
