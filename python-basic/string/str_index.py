ffb8fa396f46428ec4dc8c4a74a4095af22bd693
nkhn37 <nkhn37.github@gmail.com>
2022-03-12 15:02:40 +0900

    文字列基礎　ソースコード（インデックス／スライス）
    インデックス／スライスで部分文字列を取得する方法に関するソースコードサンプル

---
+++ python-basic/string/str_index.py
@@ -0,0 +1,12 @@
+"""文字列基礎
+インデックスで特定位置の文字列を取得する
+
+[説明ページ]
+https://tech.nkhn37.net/python-index-slice/#i-3
+"""
+# インデックスで特定位置の文字を取得する
+data = 'abcdefghijklmnopqrstuvwxyz'
+print(data[0])
+print(data[1])
+print(data[2])
+print(data[25])
