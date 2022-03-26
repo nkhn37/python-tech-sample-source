5b3f1a968f2992b4fbdf8cbe15a6899cc9a7787d
nkhn37 <nkhn37.github@gmail.com>
2022-03-27 07:15:02 +0900

    文字列基礎 ソースコード（文字列とバイト型を相互に変換する）
    文字列とバイト型を相互に変換するencode/decodeメソッドの使い方に関するソースコードサンプル。

---
+++ python-basic/string/encode_decode/decode_error.py
@@ -0,0 +1,18 @@
+"""文字列基礎
+バイトを文字列に変換する decodeメソッド (bytes -> str)
+文字コードを間違うとエラー (デフォルトでは、errors=strictになっている)
+
+[説明ページ]
+https://tech.nkhn37.net/python-encode-decode/#_decode_bytesstr
+"""
+data = 'おはようございます。'
+
+# encodeで指定文字コードにエンコードする
+encode_data = data.encode('shift_jis')
+print(encode_data)
+
+print('================================')
+# decodeで文字列に戻す
+decode_data = encode_data.decode('utf_8')
+print(decode_data)
+print(type(decode_data))
---
+++ python-basic/string/encode_decode/encode.py
@@ -0,0 +1,12 @@
+"""文字列基礎
+文字列をバイトに変換する encodeメソッド (str -> bytes)
+
+[説明ページ]
+https://tech.nkhn37.net/python-encode-decode/#_encode_strbytes
+"""
+data = 'おはようございます。'
+
+# encodeで指定文字コードにエンコードする
+encode_data = data.encode('shift_jis')
+print(encode_data)
+print(type(encode_data))
