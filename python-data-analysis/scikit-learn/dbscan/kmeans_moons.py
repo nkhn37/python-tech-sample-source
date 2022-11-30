6dfb2f957c76f9bf15d6d508216a9e89b8a04205
nkhn37 <nkhn37.github@gmail.com>
2022-12-01 06:30:24 +0900

    scikit-learn ソースコード（DBSCAN）
    DBSCANによるクラスタリングの使い方に関するソースコードサンプル

---
+++ python-data-analysis/scikit-learn/dbscan/kmeans_moons.py
@@ -0,0 +1,38 @@
+"""DBSCANとの比較
+(KMeansでmoonsデータをクラスタリングした場合)
+
+[説明ページ]
+https://tech.nkhn37.net/scikit-learn-dbscan/#k-means
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_moons
+
+
+def main():
+    """メイン関数"""
+    np.random.seed(1)
+
+    # ===== データの生成
+    data, label = make_moons(n_samples=500, noise=0.05)
+
+    # ===== KMeansでクラスタリング
+    k = 2
+    km = KMeans(n_clusters=k)
+    pred = km.fit_predict(data)
+
+    # ===== クラスタリング結果を描画する
+    fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+    # 元データを表示する
+    ax[0].scatter(data[:, 0], data[:, 1])
+    ax[0].set_title("original data")
+    # クラスタリング結果を表示する
+    for i in range(k):
+        ax[1].scatter(data[pred == i, 0], data[pred == i, 1])
+    ax[1].set_title("k-means clustering")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
