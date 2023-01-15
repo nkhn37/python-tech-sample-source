77c99eb436ed34d578310e0204e0007793e0ac22
nkhn37 <nkhn37.github@gmail.com>
2023-01-15 09:58:25 +0900

    リスト基礎 ソースコード（ソート）
    ソートに関するソースのプログラム修正とフォルダ移動

---
+++ python-basic/list/sort/sorted.py
@@ -0,0 +1,11 @@
+"""リスト基礎
+sorted関数を使用する場合（昇順）
+
+[説明ページ]
+https://tech.nkhn37.net/python-list-sort/#sorted
+"""
+# リストを昇順に並べ替えたリストを返却する。（sorted関数）
+data = [15, 5, 20, 1, 10, 25]
+sorted_data = sorted(data)
+print(f"data: {data}")
+print(f"sorted_data: {sorted_data}")
