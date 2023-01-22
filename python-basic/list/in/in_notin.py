2011d34ae962d6c342d125de5fa81b386f92728a
nkhn37 <nkhn37.github@gmail.com>
2023-01-23 06:11:00 +0900

    リスト基礎ソースコード（in, not in）
    リスト内の要素有無を確認する方法に関するソースのフォルダ変更と一部修正

--- python-basic/list/in/in_notin.py
+++ python-basic/list/in/in_notin.py
@@ -4,14 +4,14 @@
 [説明ページ]
 https://tech.nkhn37.net/python-list-in-notin/
 """
-data = ['A', 'B', 'C', 'D', 'E']
+data = ["A", "B", "C", "D", "E"]
 
 # リスト内に特定の要素が存在することを確認する。
-print('=== in ===')
-print('A' in data)
-print('Z' in data)
+print("=== in ===")
+print("A" in data)
+print("Z" in data)
 
 # リスト内に特定の要素が存在しないことを確認する
-print('\n=== not in ===')
-print('A' not in data)
-print('Z' not in data)
+print("\n=== not in ===")
+print("A" not in data)
+print("Z" not in data)
