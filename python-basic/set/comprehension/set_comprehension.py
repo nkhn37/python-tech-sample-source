6eb6e4f0a664f3741a0ba48b50c14598b724bf3f
nkhn37 <nkhn37.github@gmail.com>
2023-01-28 06:32:25 +0900

    集合基礎 ソースコード（集合内包表記）
    集合内包表記に関するソースのフォルダ変更とフォーマット修正

--- python-basic/set/comprehension/set_comprehension.py
+++ python-basic/set/comprehension/set_comprehension.py
@@ -2,7 +2,7 @@
 集合内包表記の使い方
 
 [説明ページ]
-https://tech.nkhn37.net/python-set-comprehension/#i
+https://tech.nkhn37.net/python-set-comprehension/#i-2
 
 [内包表記まとめページ]
 https://tech.nkhn37.net/python-comprehension/
@@ -11,4 +11,4 @@ data = [10, 15, 20, 25, 30, 35, 40, 45, 50, 10, 15, 20, 25, 30]
 
 # 集合内包表記
 data_set = {dt for dt in data if dt % 2 == 0}
-print(f'data_set : {data_set}')
+print(f"data_set : {data_set}")
