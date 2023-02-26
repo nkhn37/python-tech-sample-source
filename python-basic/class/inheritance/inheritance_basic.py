d0ef773aebbd1555257eb6d4353c1b52381ba16a
nkhn37 <nkhn37.github@gmail.com>
2023-02-26 10:43:54 +0900

    クラス基礎 ソースコード （クラスの継承の基本）修正
    フォルダ移動およびコメントアウト、フォーマットの修正

--- python-basic/class/inheritance/inheritance_basic.py
+++ python-basic/class/inheritance/inheritance_basic.py
@@ -2,31 +2,35 @@
 クラスの継承の基本
 
 [説明ページ]
-https://tech.nkhn37.net/python-class-inheritance/#i
+https://tech.nkhn37.net/python-class-inheritance/#Python
 """
 
 
-class Person(object):
+class Person:
+    """人間クラス"""
+
     def __init__(self, first_name=None, last_name=None):
         self.first_name = first_name
         self.last_name = last_name
 
     def say_myname(self):
-        print(f'私の名前は、{self.last_name}{self.first_name}です。')
+        print(f"私の名前は、{self.last_name}{self.first_name}です。")
 
 
 class Teacher(Person):
+    """教師クラス"""
+
     def mywork(self):
-        print(f'{self.last_name}{self.first_name}は教師として働いています。')
+        print(f"{self.last_name}{self.first_name}は教師として働いています。")
 
 
 def main():
-    tanaka = Teacher('太郎', '田中')
+    tanaka = Teacher("太郎", "田中")
     # 親クラスのメソッドの呼び出し
     tanaka.say_myname()
     # 自分のクラスのメソッドの呼び出し
     tanaka.mywork()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- python-basic/class/inheritance/inheritance_super.py
+++ python-basic/class/inheritance/inheritance_super.py
@@ -6,16 +6,20 @@ https://tech.nkhn37.net/python-class-inheritance/#super
 """
 
 
-class Person(object):
+class Person:
+    """人間クラス"""
+
     def __init__(self, first_name=None, last_name=None):
         self.first_name = first_name
         self.last_name = last_name
 
     def say_myname(self):
-        print(f'私の名前は、{self.last_name}{self.first_name}です。')
+        print(f"私の名前は、{self.last_name}{self.first_name}です。")
 
 
 class Teacher(Person):
+    """教師クラス"""
+
     def __init__(self, first_name=None, last_name=None, subject=None):
         # 親クラスのコンストラクタの呼び出し
         super().__init__(first_name, last_name)
@@ -23,18 +27,17 @@ class Teacher(Person):
         self.subject = subject
 
     def mywork(self):
-        print(f'{self.last_name}{self.first_name}は教師として働いています。')
+        print(f"{self.last_name}{self.first_name}は教師として働いています。")
 
     def say_myname(self):
-        print(f'私は、{self.subject}教師の{self.last_name}'
-              f'{self.first_name}です。')
+        print(f"私は、{self.subject}教師の{self.last_name}" f"{self.first_name}です。")
 
 
 def main():
-    tanaka = Teacher('太郎', '田中', '英語')
+    tanaka = Teacher("太郎", "田中", "英語")
     tanaka.say_myname()
     tanaka.mywork()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
