19b5b8f9828d52c97fd8630c0c68ad59a909d043
nkhn37 <nkhn37.github@gmail.com>
2023-03-24 07:08:04 +0900

    configparser ソースコード修正
    フォーマット修正と説明ページ修正に伴うリンク先変更

--- python-libraries/configparser/configparser_basic.py
+++ python-libraries/configparser/configparser_basic.py
@@ -2,31 +2,25 @@
 基本的な使い方
 
 [説明ページ]
-https://tech.nkhn37.net/python-configparser-basic/#configparserconfig-2
+https://tech.nkhn37.net/python-configparser-basic/#configparser-2
 """
 import configparser
 
 config = configparser.ConfigParser()
-config['WEB_SERVER'] = {
-    'HOST': 'xxx.xxx.xxx.xxx',
-    'PORT': 80
-}
-config['DB_SERVER'] = {
-    'HOST': 'xxx.xxx.xxx.xxx',
-    'PORT': 3306
-}
+config["WEB_SERVER"] = {"HOST": "xxx.xxx.xxx.xxx", "PORT": 80}
+config["DB_SERVER"] = {"HOST": "xxx.xxx.xxx.xxx", "PORT": 3306}
 
 # configファイルへの書き込み
-with open('config.ini', 'w') as config_file:
+with open("config.ini", "w") as config_file:
     config.write(config_file)
 
 # configファイルからの読み込み
 config1 = configparser.ConfigParser()
-config1.read('config.ini')
+config1.read("config.ini")
 # 読み込んだ結果を参照
-print(config1['WEB_SERVER'])
-print(config1['WEB_SERVER']['HOST'])
-print(config1['WEB_SERVER']['PORT'])
-print(config1['DB_SERVER'])
-print(config1['DB_SERVER']['HOST'])
-print(config1['DB_SERVER']['PORT'])
+print(config1["WEB_SERVER"])
+print(config1["WEB_SERVER"]["HOST"])
+print(config1["WEB_SERVER"]["PORT"])
+print(config1["DB_SERVER"])
+print(config1["DB_SERVER"]["HOST"])
+print(config1["DB_SERVER"]["PORT"])
