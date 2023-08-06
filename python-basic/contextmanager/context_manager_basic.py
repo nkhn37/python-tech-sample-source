6e80bc67ee1b7d2675e4091a7d59d5719b56973d
nkhn37 <nkhn37.github@gmail.com>
2023-08-06 12:20:10 +0900

    コンテキストマネージャーの基本 ソースコード 修正
    ・フォーマットの修正
    ・例外発生時の表示修正
    ・後続処理を示すprintの追加

--- python-basic/contextmanager/context_manager_basic.py
+++ python-basic/contextmanager/context_manager_basic.py
@@ -10,30 +10,32 @@ https://tech.nkhn37.net/python-context-manager-basic/
 class SampleContext:
     # コンテキストを作成する
     def __enter__(self):
-        print('--- コンテキスト開始(enter) ---')
+        print("--- コンテキスト開始(enter) ---")
         return self
 
     # コンテキストを終了する
     def __exit__(self, exception_type, exception_value, traceback):
-        print('--- コンテキスト終了(exit) ---')
+        print("--- コンテキスト終了(exit) ---")
         if exception_type is None:
-            print('エラーなし')
+            print("エラーなし")
         else:
-            print(f'エラー: {exception_value}')
+            print(f"エラー: {exception_type}, {exception_value}, {traceback}")
             return True
 
     # 任意の関数
     @staticmethod
     def test():
-        print('testメソッド')
+        print("testメソッド")
 
 
 def main():
     with SampleContext() as sample:
         sample.test()
         # # 何かしらエラーが発生すると挙動が変わる
-        # raise ValueError('不正な値')
+        # raise ValueError("不正な値")
+
+    print("後続処理")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
