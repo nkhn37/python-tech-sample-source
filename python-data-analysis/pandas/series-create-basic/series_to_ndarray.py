ea07e48a154aa248a353414ff40db198cfb568af
NaokiHoshino <nao.bright37star@gmail.com>
2023-10-12 06:27:46 +0900

    pandas ソースコード（Seriesの作成方法）(修正)
    Seriesの作成をシンプルにして、Series⇒ndarrayへの変換が本プログラムで説明したい中心であることを分かるように修正。

--- python-data-analysis/pandas/series-create-basic/series_to_ndarray.py
+++ python-data-analysis/pandas/series-create-basic/series_to_ndarray.py
@@ -7,10 +7,9 @@ https://tech.nkhn37.net/pandas-series-create-basic/#SeriesNumPyndarray
 import numpy as np
 import pandas as pd
 
-# NumPy ndarrayからSeriesへ変換する
-arr = np.array([1, 5, 2, 4, 5])
-data = pd.Series(arr)
-print(data, '\n')
+# Seriesを作成する
+data = pd.Series([1, 5, 2, 4, 5])
+print(data, "\n")
 
 # SeriesからNumPy ndarrayへ変換する
 arr_tmp = data.values
