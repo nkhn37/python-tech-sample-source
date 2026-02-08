d3db3a8da0834314cec15f26fa080f57e42887ff
nkhn37 <nkhn37.github@gmail.com>
2026-02-08 10:43:29 +0900

    ctypes で外部DLLを使用する方法 (修正)
    以下の点を修正
    - user32.dll のみで実装できるので kernel32.dll を削除
    - 呼び出し先にあわせて、変数名を MessageBox から MessageBoxW に修正
    - 型指定を wintypes を使用する方法に修正
    - エラー処理を ctypes.get_last_error や ctypes.WinError を使用した方法に修正

--- python-libraries/ctypes/ctypes_windows_api.py
+++ python-libraries/ctypes/ctypes_windows_api.py
@@ -1,32 +1,30 @@
 import ctypes
+from ctypes import wintypes
 
-# user32.dllを読み込む
-user32 = ctypes.WinDLL("user32.dll", use_last_error=True)
-# kernel32.dllを読み込む
-kernel32 = ctypes.WinDLL("kernel32.dll", use_last_error=True)
-
+# user32.dllを読み込む (.dll拡張子は省略可能)
+user32 = ctypes.WinDLL("user32", use_last_error=True)
 
 # MessageBoxを定義
-MessageBox = user32.MessageBoxW
-MessageBox.argtypes = [
-    ctypes.c_void_p,  # HWND (ウィンドウハンドルを示す識別子)
-    ctypes.c_wchar_p,  # LPCTSTR (メッセージのテキスト)
-    ctypes.c_wchar_p,  # LPCTSTR (メッセージボックスのタイトル)
-    ctypes.c_uint,  # UINT (メッセージボックスのスタイル)
+MessageBoxW = user32.MessageBoxW
+MessageBoxW.argtypes = [
+    wintypes.HWND,  # HWND (ウィンドウハンドルを示す識別子)
+    wintypes.LPCWSTR,  # LPCWSTR (メッセージのテキスト)
+    wintypes.LPCWSTR,  # LPCWSTR (メッセージボックスのタイトル)
+    wintypes.UINT,  # UINT (メッセージボックスのスタイル)
 ]
-MessageBox.restype = ctypes.c_int  # INT (返却値)
-
-# GetLastErrorを定義
-GetLastError = kernel32.GetLastError
-GetLastError.argtypes = []
-GetLastError.restype = ctypes.c_ulong
+MessageBoxW.restype = wintypes.INT  # 戻り値の型
 
 # メッセージボックスを表示
-result = MessageBox(0, "Hello, World!", "From Python", 0)
+result = MessageBoxW(None, "Hello, World!", "From Python", 0)
+
+# # 以下のように無効なウィンドウハンドルでは、エラーとなる。（エラーを発生の確認例）
+# result = MessageBoxW(
+#     wintypes.HWND(12345678), "Hello, World!", "From Python", 0
+# )
 
 # 戻り値をチェックしてエラー処理
 if result == 0:
-    error_code = GetLastError()
-    print(f"MessageBox failed with error code: {error_code}")
+    err = ctypes.get_last_error()
+    raise ctypes.WinError(err)
 else:
     print(f"MessageBox return: {result}")
