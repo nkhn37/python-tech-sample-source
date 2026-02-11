e723ab4b2c4d2f1f520a61363d86cc5eaa7d7c96
nkhn37 <nkhn37.github@gmail.com>
2026-02-12 05:35:30 +0900

    CFFI で外部DLLを使用する方法 (修正)
    以下の点を修正
    - 返却値でのエラー処理を追加
    - 変数は `ffi.new` を使用して生成するように変更
    - 拡張子は省略可能な点を反映
    - `cdef` の宣言内のコメント (//) は環境により動作しない可能性があるので削除

--- python-libraries/cffi/cffi_windows_api.py
+++ python-libraries/cffi/cffi_windows_api.py
@@ -12,18 +12,43 @@ ffi = FFI()
 # 関数の宣言
 ffi.cdef(
     """
-    // メッセージボックス
     int MessageBoxW(void *hwnd, const wchar_t *text, const wchar_t *caption, unsigned int type);
-"""
+    """
 )
 
-# DLLのロード
-user32 = ffi.dlopen("user32.dll")
+# DLLのロード (拡張子は省略可能)
+user32 = ffi.dlopen("user32")
 
 try:
-    # MessageBoxの表示
-    result = user32.MessageBoxW(ffi.NULL, "Hello, World!", "From Python", 0)
+    # 変数の生成
+    text = ffi.new("wchar_t[]", "Hello, World!")
+    caption = ffi.new("wchar_t[]", "From Python")
+
+    # メッセージボックスの表示 (ffi.NULL は ffi.cast("void*", 0) と同等)
+    result = user32.MessageBoxW(ffi.NULL, text, caption, 0)
+
+    # --- 以下はエラーが発生する例 ---
+    # 以下のようにすると型エラーとなる
+    # result = user32.MessageBoxW(ffi.NULL, 123, 456, 0)
+
+    # 以下のように無効な HWND を指定するとWindows APIエラーとなる
+    # result = user32.MessageBoxW(
+    #     ffi.cast("void*", 0x12345678), text, caption, 0
+    # )
+    # ------------------------------------
+
+    # 返却値が 0 の場合はエラー
+    if result == 0:
+        # 直前の GetLastError() の値と対応するエラーメッセージを取得
+        errcode, errmsg = ffi.getwinerror()
+        raise OSError(errcode, errmsg)
+
     print(f"MessageBox return: {result}")
 
+except OSError as ex:
+    # Windows API 呼び出しでエラーが発生した場合
+    print(f"Windows API エラー: {ex}")
+
 except TypeError as ex:
-    print(f"An error occurred: {ex}")
+    # 引数の型が不正な場合
+    print(f"引数の型エラー: {ex}")
